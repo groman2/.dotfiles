dc18cf5fd924e45b9a864e8e50556fd4f1510731
Yuriy Romanenko <yuriy@lytro.com>
2016-09-23 09:28:23 -0700

    Changed the way setup.sh works and updated to my .dotfiles

--- setup.sh
+++ setup.sh
@@ -6,8 +6,15 @@ set -x -e
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # link up git dotfiles to home dotfiles
-ln -sf $dir/.bash_profile ~/.bash_profile
-ln -sf $dir/.gitconfig ~/.gitconfig
+ln -sf $dir/gitconfig ~/.gitconfig
+ln -vsf $dir/profile ~/.profile
+ln -vsf $dir/inputrc ~/.inputrc
+ln -vsf $dir/bashrc ~/.bashrc
+ln -vsf $dir/gitignore ~/.config/git/ignore
+for x in $dir/bashrc.*; do
+    ln -vsf $x ~/.$(basename $x)
+done
+
 
 # install brew of some kind
 # OSX: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -19,11 +26,15 @@ PATH="$HOME/.linuxbrew/bin:$PATH"
 brew install neovim/neovim/neovim
 
 # verify this works when there's already shit here...
-rm -rf ~/.config/nvim
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim.$(date +%s)
+fi
 mkdir -p ~/.config/
+
 ln -s $dir/vim ~/.config/nvim
 
 # command line utilities
+brew install python
 brew install git
 brew install fasd
 brew install ag
@@ -31,5 +42,7 @@ brew install jq
 brew install aria2
 brew install bash-git-prompt
 
-brew tap universal-ctags/universal-ctags
-brew install universal-ctags/universal-ctags
+#brew tap universal-ctags/universal-ctags
+#brew install universal-ctags/universal-ctags/universal-ctags
+
+#brew install homebrew/dupes/openssh
