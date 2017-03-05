e064a68cf83dfd76afac4d0b5a27646aaf5ec4b0
Yuriy Romanenko <yuriy@lytro.com>
2017-03-05 18:21:03 +0000

    Added "VM" mode to setup.sh

--- setup.sh
+++ setup.sh
@@ -38,30 +38,84 @@ for x in $dir/bashrc.*; do
     ln -vsf $x ~/.$(basename $x)
 done
 
+yesbrew=1
+yespip=1
+yespylab=1
 
+while [[ ! -z $1 ]]; do
+   case "$1" in
+       nobrew)
+           unset yesbrew
+           nobrew=1
+       ;;
+
+       nopip)
+           unset yespip
+           nopip=1
+       ;;
+
+       nopylab)
+           unset yespylab
+           nopylab=1
+       ;;
+
+       vm)
+          echo VM Mode - no brew or pylab
+          unset yesbrew
+          unset yespylab
+          nobrew=1
+          nopylab=1
+       ;;
+    esac
+    shift
+done
+
+if [[ -z $yesbrew ]]; then
+    echo Running in no-brew mode, try installing packages systemwide manually
+    echo Xenial:
+    echo   sudo add-apt-repository ppa:neovim-ppa/stable
+    echo   sudo apt-get update
+    echo   sudo apt-get install neovim jq aria2
+    echo
+    echo
+fi
 # install brew of some kind
 # OSX: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # linux:
-if [[ $PLATFORM == *"linux"* ]]; then
-   if [ ! -e $HOME/.linuxbrew/bin/brew ]; then
-     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-   fi
-   PATH="$HOME/.linuxbrew/bin:$PATH"
-elif [[ $PLATFORM == *"darwin"* ]]; then
-   if [ ! -e /usr/local/bin/brew ]; then
-     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-   fi
-else
-   echo Could not figure out what platform this is: $PLATFORM
-   echo Exiting before bad things happen
-   exit 1
+if [[ -z $nobrew ]]; then
+    if [[ $PLATFORM == *"linux"* ]]; then
+       if [ ! -e $HOME/.linuxbrew/bin/brew ]; then
+          ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+       fi
+       PATH="$HOME/.linuxbrew/bin:$PATH"
+    elif [[ $PLATFORM == *"darwin"* ]]; then
+       if [ ! -e /usr/local/bin/brew ]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    else
+       echo Could not figure out what platform this is: $PLATFORM
+       echo Exiting before bad things happen
+       exit 1
+    fi
+fi
+
+if ! which brew || [[ -z $yesbrew ]]; then
+    function brew() {  echo skipping brew $*; }
+    nobrew=1
+    unset yesbrew
+fi
+
+if ! which pip || [[ -z $yespip ]]; then
+    function pip() { echo skipping pip $*; }
+    nopip=1
+    unset yespip
 fi
 
 # Nope, bad idea
 #if ! $(which g++) && ! (which clang++); then
 #  brew install gcc
 #fi
-if ! which make || ! which c++; then
+if [[ -z $nobrew ]] && {  ! which make || ! which c++; }; then
   echo Cannot install neovim and other brew packages because basic make/c++ packages are not installed
   exit 1
 fi
@@ -97,7 +151,12 @@ mkdir -p ~/.config/
 ln -s $dir/vim ~/.config/nvim
 
 
-pip install jinja2 pyyaml editdistance pycrypto ipython sympy rlcompleter3 numpy scipy matplotlib
+pip install --user jinja2 pyyaml editdistance pycrypto rlcompleter3 
+
+if [[ -z $nopylab ]]; then
+    pip install --user ipython sympy numpy scipy matplotlib
+fi
+
 #brew tap universal-ctags/universal-ctags
 #brew install universal-ctags/universal-ctags/universal-ctags
 
